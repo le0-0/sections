ac44610deafd30cdddf7d79306542f993dabc471
le0-0 <leonard.schoyen@gmail.com>
2022-07-05 13:39:26 +0200

    Initial csharp solution.

---
+++ development/ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+	/// <summary>
+	/// Tje class containing code entry point.
+	/// </summary>
+	internal class Program
+	{
+		/// <summary>
+		/// The code entry point.
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
